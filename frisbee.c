8b7944de9e38fd2ebb85630694ceaa71d7ac6ca6
I Hsien Huang <ihuan010@sledge.cs.ucr.edu>
2017-11-29 13:43:21 -0800

    a small game to test clone and thread

--- frisbee.c
+++ frisbee.c
@@ -3,6 +3,9 @@
 #include "user.h"
 int main ( int argc , char * argv [])
 {
-	clone();
+	int size = 4096;
+	int child_pid = clone(size);
+	if(child_pid ==1)
+		child_pid = clone(size);
 	exit ();
 }
