3bc122499d6fecb7dc242e8c995506038eca8299
I Hsien Huang <ihuan010@sledge.cs.ucr.edu>
2017-11-30 14:15:58 -0800

    frisbee game

--- frisbee.c
+++ frisbee.c
@@ -5,13 +5,25 @@ int main ( int argc , char * argv [])
 {
 	int size = 4096;
 
-	int i = 0 ;
-	for(i = 0 ; i < 5; i ++)
-	{	
-		int* stack = (int*)malloc(size*sizeof(int));
-		int child_pid = clone(stack,size);
-		if(child_pid ==1)
-			child_pid = clone(stack,size);	
-	}
+//	int i = 0 ;
+//	for(i = 0 ; i < 1; i ++)
+//	{	
+		int* stack[3];
+		int i = 0 ;
+		for(i=0;i<3;i++)
+		{
+			stack[i]  = (int*)malloc(size*sizeof(int));
+		}
+		int child_pid = clone(stack[0],size);
+		if(child_pid !=0)
+		{
+			child_pid = clone(stack[1],size);
+		}
+		if(child_pid !=0)
+			printf(1,"@@%d\n",child_pid);
+		if(child_pid ==0)
+			printf(1,"this is child%d\n",child_pid);
+//	}
+	wait();
 	exit ();
 }
