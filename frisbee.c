d451d7555646c2719dfc93dd565d44cd8dfdb98c
I Hsien Huang <ihuan010@sledge.cs.ucr.edu>
2017-11-30 15:04:29 -0800

    add thread library

--- frisbee.c
+++ frisbee.c
@@ -1,29 +1,29 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "thread.h"
 int main ( int argc , char * argv [])
 {
+	
 	int size = 4096;
-
-//	int i = 0 ;
-//	for(i = 0 ; i < 1; i ++)
-//	{	
-		int* stack[3];
-		int i = 0 ;
-		for(i=0;i<3;i++)
-		{
-			stack[i]  = (int*)malloc(size*sizeof(int));
-		}
-		int child_pid = clone(stack[0],size);
-		if(child_pid !=0)
-		{
-			child_pid = clone(stack[1],size);
-		}
-		if(child_pid !=0)
-			printf(1,"@@%d\n",child_pid);
-		if(child_pid ==0)
-			printf(1,"this is child%d\n",child_pid);
-//	}
+	int* stack[3];
+	int i = 0 ;
+	printf(1,"@@@@@@@@ %d\n",PGSIZE);
+	for(i=0;i<3;i++)
+	{
+		stack[i]  = (int*)malloc(size*sizeof(int));
+	}
+	int child_pid = clone(stack[0],size);
+	if(child_pid !=0)
+	{
+		child_pid = clone(stack[1],size);
+	}
+	if(child_pid !=0)
+		printf(1,"@@%d\n",child_pid);
+	if(child_pid ==0)
+		printf(1,"this is child%d\n",child_pid);
+	
+	wait();
 	wait();
 	exit ();
 }
