a4a3f8207598ad8ea3ac0d9e6e289579e5a1672f
I-HSIEN HUANG <isytkr34@gmail.com>
2017-12-01 11:33:48 -0800

    frisbee for spin_lock final version

--- frisbee.c
+++ frisbee.c
@@ -18,8 +18,7 @@ int main ( int argc , char * argv [])
 	for(i=0;i<numofthread;i++)
 	{
 		//	stack[i]  = (int*)malloc(size*sizeof(int));
-		thread_create(worker,(void *)0);
-
+		thread_create(worker,(void *)i);
 	}
 	wait();
 	exit();
@@ -34,28 +33,32 @@ int main ( int argc , char * argv [])
 #if DEBUD
 	printf(1,"lock_test = %d\n",lock->locked);
 #endif 
-	/*lock_acquire(lock);
-	  printf(1,"output : %d\n",output);
-	  output++;
-	  numofthread--;
-	  lock_release(lock);	
-	  */
+
 }
-void* worker()
+/*worker is thr function passed into the threads
+ * each thread after created will do the function
+ * arg is the parameter passed into the thread_create 
+ * and make "arg" as the id of each thread 
+ * in while loop every thread acquire the lock and the thread get the lock stil need to check pid num
+ * 1 pass token to 2,2 to 3 and so on.
+ * if the thread get the lock does not the thread should work 
+ * it will sleep and release the lock 
+ * */
+void* worker(void *arg)
 {
+	
 	//lock_init(&ttable.lock);
-	int pidnum = getpid()-4;
+	int pidnum = (int)arg;
 #if DEBUG
 	printf(1,"child's pid num is %d\n",pidnum);
 #endif
-	while(output<=passnum)//when pass time  bigger than it should be passed
+	while(output<passnum)//when pass time  bigger than it should be passed
 	{
 		lock_acquire(lock);
-		if(output>passnum)
+		if(output==passnum)
 			break;
 		if(pidnum==workpid)
 		{
-			
 			output++;
 			printf(1,"pass number no:%d is thread %d is passing the token to ",output,pidnum);
 			workpid++;
